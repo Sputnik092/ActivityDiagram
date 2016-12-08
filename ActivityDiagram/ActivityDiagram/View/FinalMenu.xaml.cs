7d085470d385d211fc2f755a0ebee8c2bc8e60c9
Mkiza <Mkiza@RONALDMKIZA5889>
2016-12-08 17:16:00 +0100

    Visual

---
+++ ActivityDiagram/ActivityDiagram/View/FinalMenu.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ActivityDiagram.View
+{
+    /// <summary>
+    /// Interaction logic for FinalMenu.xaml
+    /// </summary>
+    public partial class FinalMenu : UserControl
+    {
+        public FinalMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ActivityDiagram/ActivityDiagram/View/MainWindow.xaml.cs
+++ ActivityDiagram/ActivityDiagram/View/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System.Windows;
 using ActivityDiagram.ViewModel;
+using System.Windows.Controls.Ribbon;
 
 namespace ActivityDiagram
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
